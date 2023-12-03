2bc2af2c97473729d7e08c96261734447763631e
Paul Beardwell <beardwell@hotmail.com>
2023-12-03 21:40:09 +0000

    Deleted class comment from disorder app

--- disorders/urls.py
+++ disorders/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import DisorderListView, DisorderDetailView
+from .views import DisorderListView, DisorderDetailView, AddCommentView, DeleteCommentView
 
 urlpatterns = [
     path('disorders/', DisorderListView.as_view(), name='disorder-list'),
     path('disorders/', DisorderDetailView.as_view(), name='disorder-detail'),
+    path('disorder/<int:disorder_id>/comment/add/', AddCommentView.as_view(), name='add_comment'),
+    path('disorder/<int:disorder_id>/comment/<int:pk>/delete/', DeleteCommentView.as_view(), name='delete_comment'),
 ]
