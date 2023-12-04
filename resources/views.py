f092600933585ccd3a19162a9eba8737aeccdd39
Paul Beardwell <beardwell@hotmail.com>
2023-12-04 00:17:11 +0000

    Only superuser can use therapist registration form

--- resources/views.py
+++ resources/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponseForbidden
 from django.shortcuts import render, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views import View
 from django.views.generic import ListView, FormView, DeleteView
 from .models import Therapist
@@ -33,10 +34,18 @@ class TherapistByLocationListView(ListView):
         return context
 
 
-class TherapistRegistrationView(FormView):
+class TherapistRegistrationView(UserPassesTestMixin, FormView):
     template_name = 'therapist_form.html'
     form_class = TherapistForm
     success_url = reverse_lazy('therapist_list')
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        # Customize behavior when the user doesn't have permission (redirect, show error, etc.)
+        # Here, redirecting to a different page or displaying an error message
+        return HttpResponseForbidden("You don't have permission to access this page. Please contact site admin if you wish to register as a therapist.")
 
     def form_valid(self, form):
         form.save()
