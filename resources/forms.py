258faed6287ba9052742d96aff7743d55d68ef94
Paul Beardwell <beardwell@hotmail.com>
2023-12-06 21:05:13 +0000

    Update form now has placeholder text prefilled with current entries. Styling to form boxes - padding bottom

--- resources/forms.py
+++ resources/forms.py
@@ -9,17 +9,28 @@ class TherapistForm(forms.ModelForm):
         model = Therapist
         fields = ['username', 'email', 'profile_pic', 'phone_number', 'address', 'bio', 'location', 'specialty']
 
+class TherapistUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Therapist
+        fields = ['username', 'email', 'profile_pic', 'phone_number', 'address', 'bio', 'location', 'specialty']
+
+    def __init__(self, *args, **kwargs):
+        super(TherapistUpdateForm, self).__init__(*args, **kwargs)
+        
+        # Iterate through fields and set placeholders
+        for field_name, field in self.fields.items():
+            if field_name == 'profile_pic':
+                # You might not need a placeholder for an image field
+                continue
+
+            current_value = self.instance.__dict__.get(field_name)
+            if current_value:
+                field.widget.attrs['placeholder'] = current_value
 
 # class TherapistUpdateForm(forms.ModelForm):
 #     class Meta:
 #         model = Therapist
 #         fields = ['username', 'email', 'profile_pic', 'phone_number', 'address', 'bio', 'location', 'specialty']
-
-
-class TherapistUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Therapist
-        fields = ['username', 'email', 'profile_pic', 'phone_number', 'address', 'bio', 'location', 'specialty']
 
 
 # I wasn't able to get the update form fields
