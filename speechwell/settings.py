ce9fbb371fd05817cc2e2d10535a3341b875f66c
Paul Beardwell <beardwell@hotmail.com>
2023-12-06 22:37:00 +0000

    Setup debug env for deployment. Form-container class added to therapist_update form

--- speechwell/settings.py
+++ speechwell/settings.py
@@ -31,8 +31,8 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = [
     '8000-beardyone80-speechwell-1ke4es9kiwq.ws-eu106.gitpod.io',
