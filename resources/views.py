5f2f7d0c2155bcf5355d767c05ca94b98d055ef0
Paul Beardwell <beardwell@hotmail.com>
2023-12-06 18:03:26 +0000

    Allow staff to add records via therapist registration form

--- resources/views.py
+++ resources/views.py
@@ -37,11 +37,12 @@ class TherapistRegistrationView(UserPassesTestMixin, FormView):
     success_url = reverse_lazy('therapist_list')
 
     def test_func(self):
-        return self.request.user.is_superuser
+        user = self.request.user
+        return user.is_superuser or user.is_staff
+        
 
     def handle_no_permission(self):
-        # Customize behavior when the user doesn't have permission (redirect, show error, etc.)
-        # Here, redirecting to a different page or displaying an error message
+        # Error message if user is not superuser
         return HttpResponseForbidden("You don't have permission to access this page. Please contact site admin if you wish to register as a therapist.")
 
     def form_valid(self, form):
@@ -61,38 +62,9 @@ class DeleteTherapistView(View):
     def post(self, request, username):
         therapist = get_object_or_404(Therapist, username=username)
         if 'confirm_delete' in request.POST:
-            therapist.delete()  # Delete the therapist object from the database
-        return redirect('therapist_list')  # Redirect to therapist list after deletion
-      
-
-
-# class DeleteTherapistView(View):
-#     def post(self, request, username):
-#         therapist = get_object_or_404(Therapist, username=username)
-#         therapist.delete()
-#         return HttpResponseRedirect(reverse('therapist_list'))
-
-# class DeleteTherapistView(View):
-#     def post(self, request, username):
-#         therapist = get_object_or_404(Therapist, username=username)
-#         return render(request, 'confirm_delete_therapist.html', {'therapist': therapist})
-
-# class DeleteTherapistView(View):
-#     def get(self, request, username):
-#         therapist = get_object_or_404(Therapist, username=username)
-#         return render(request, 'delete_therapist.html', {'therapist': therapist})
-
-#     def post(self, request, username):
-#         therapist = get_object_or_404(Therapist, username=username)
-#         if request.POST.get('confirm_delete'):
-#             therapist.delete()  # Delete the therapist object from the database
-#             return redirect('therapist_list')  # Redirect to therapist list after deletion
-#         else:
-#             # Handle cancellation or any other action upon not confirming deletion
-#             return redirect('therapist_list')  # Redirect to therapist list without deletion         
-
-
-
+            therapist.delete()
+        return redirect('therapist_list')
+    
 
 class TherapistUpdateView(UpdateView):
     model = Therapist
@@ -106,39 +78,6 @@ class TherapistUpdateView(UpdateView):
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        instance = self.get_object()  # Retrieve the instance here
+        instance = self.get_object()
         kwargs['instance'] = instance
-        print(instance.username)  # Print some attributes of the instance for debugging
-        print(instance.email)  # Example: Print the email field  # Pass the instance to the form
         return kwargs
-
-# class TherapistUpdateView(UpdateView):
-#     model = Therapist
-#     form_class = TherapistUpdateForm
-#     template_name = 'therapist_update.html'
-#     success_url = reverse_lazy('therapist_list')
-
-#     def get_object(self, queryset=None):
-#         username = self.kwargs.get('username')
-#         return get_object_or_404(Therapist, username=username)
-
-#     def get_form_kwargs(self):
-#         kwargs = super().get_form_kwargs()
-#         instance = self.get_object()  # Retrieve the instance here
-#         kwargs['instance'] = instance  # Pass the instance to the form
-#         return kwargs        
-        
-# class TherapistUpdateView(UpdateView):
-#     model = Therapist
-#     form_class = TherapistUpdateForm
-#     template_name = 'therapist_update.html'  # Template to render the update form
-#     success_url = reverse_lazy('therapist_list')  # Replace 'therapists_list' with your list view name
-
-#     def get_object(self, queryset=None):
-#         username = self.kwargs.get('username')
-#         return get_object_or_404(Therapist, username=username)
-
-#     def get_form_kwargs(self):
-#         kwargs = super().get_form_kwargs()
-#         kwargs['instance'] = self.get_object()  # Pass the instance of the therapist to the form
-#         return kwargs
