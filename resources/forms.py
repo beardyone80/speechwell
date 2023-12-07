d11a22d54aad8c6087af95802d8a4cb38f28dbbd
Paul Beardwell <beardwell@hotmail.com>
2023-12-07 14:25:16 +0000

    Delete stray p tag and close main tag on about page for W3C checks

--- resources/forms.py
+++ resources/forms.py
@@ -27,17 +27,3 @@ class TherapistUpdateForm(forms.ModelForm):
             if current_value:
                 field.widget.attrs['placeholder'] = current_value
 
-# class TherapistUpdateForm(forms.ModelForm):
-#     class Meta:
-#         model = Therapist
-#         fields = ['username', 'email', 'profile_pic', 'phone_number', 'address', 'bio', 'location', 'specialty']
-
-
-# I wasn't able to get the update form fields
-# prepopulated
-
-    # def __init__(self, *args, **kwargs):
-    #     super(TherapistUpdateForm, self).__init__(*args, **kwargs)
-    #     self.helper = FormHelper()
-    #     self.helper.form_method = 'post'
-    #     self.helper.add_input(Submit('submit', 'Update'))
